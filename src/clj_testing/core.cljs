1e01352b881be00addf5843478675787f9b79d26
tankorsmash <tankorsmash@gmail.com>
2021-08-31 16:51:01 -0400

    try to implement devtools; mess around with maps

--- src/clj_testing/core.cljs
+++ src/clj_testing/core.cljs
@@ -1,6 +1,9 @@
 (ns clj-testing.core
   (:require-macros [hiccups.core :as hiccups :refer [html]])
-  (:require [hiccups.runtime :as hiccupsrt]))
+  (:require [hiccups.runtime :as hiccupsrt]
+           [devtools.core :as devtools]))
+
+(devtools/install!)
 
 
 ;; (use 'hiccup.core)
@@ -12,20 +15,21 @@
    [:a {:href "https://github.com/weavejester/hiccup"}
     "Hiccup"]])
 
-(defn make_span [text]
-  (html [:span {:class "foo"} text]))
+;; (defn make_span [text]
+;;   (html [:span {:class "foo"} text]))
 
 (def my-doc (js/document.getRootNode))
 
-(defn hello []
-  (html [ :h2 {} "HEADER 2"]
+(defn render_dom "takes nothing and returns a new string for the entire DOM" []
+  (html [:h2 {} "HEADER 2"]
         [:div {} "ASDSD"]
-        (make_span "ASSS2")))
+        (:span {:class "foobar"} "ASSS2dsds")))
 
-;; (def main-doc (js/document))
+(def main-doc (js/document.getRootNode))
 ;; uncomment this to alter the provided "app" DOM element
-(set! (.-innerHTML (js/document.getElementById "app")) (hello))
-;; (set! (.-innerHTML (js/document.getRootNode)) (hello))
+(set! (.-innerHTML (js/document.getElementById "app")) (render_dom))
+;; (set! (.-innerHTML (js/document.getRootNode)) (render_dom))
 
-(println (str
-            "the doc:\n" (hello)))
+(def person {:name "josh" :age 21})
+(println (str "output -|\n" (str person.name) "|- done output"))
+;;             "the doc:\n" (render_dom)))
