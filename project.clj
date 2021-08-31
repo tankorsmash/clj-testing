4597abf0fe6010a6a2dfea299a4170b4288d71d6
tankorsmash <tankorsmash@gmail.com>
2021-08-31 16:02:22 -0400

    use hiccups instead of hiccup for cljs; leave the browser opening

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
        :dependencies [[org.clojure/clojurescript "1.10.773"]
                       [com.bhauman/figwheel-main "0.2.14"]
                       [org.slf4j/slf4j-simple "1.7.5"]
-                      [hiccup "1.0.5"]
+                      ;; [hiccup "1.0.5"] ;;might only be in clj, not cljs
+                      [hiccups "0.3.0"]
                       ;; optional but recommended
                       [com.bhauman/rebel-readline-cljs "0.1.4"]]}}
   :aliases {"fig" ["trampoline" "run" "-m" "figwheel.main"]})
