636302439e06092b65090f8209be31a853442e6d
tankorsmash <tankorsmash@gmail.com>
2021-08-31 16:01:44 -0400

    play around with cljs and understand a bit more the way you pass args

--- src/clj_testing/core.cljs
+++ src/clj_testing/core.cljs
@@ -1,10 +1,31 @@
-(ns clj-testing.core)
+(ns clj-testing.core
+  (:require-macros [hiccups.core :as hiccups :refer [html]])
+  (:require [hiccups.runtime :as hiccupsrt]))
+
+
+;; (use 'hiccup.core)
 
 (enable-console-print!)
 
-(defn hello [] "hello There")
+(hiccups/defhtml my-template []
+  [:div
+   [:a {:href "https://github.com/weavejester/hiccup"}
+    "Hiccup"]])
 
+(defn make_span [text]
+  (html [:span {:class "foo"} text]))
+
+(def my-doc (js/document.getRootNode))
+
+(defn hello []
+  (html [ :h2 {} "HEADER 2"]
+        [:div {} "ASDSD"]
+        (make_span "ASSS2")))
+
+;; (def main-doc (js/document))
 ;; uncomment this to alter the provided "app" DOM element
-;; (set! (.-innerHTML (js/document.getElementById "app")) (hello))
+(set! (.-innerHTML (js/document.getElementById "app")) (hello))
+;; (set! (.-innerHTML (js/document.getRootNode)) (hello))
 
-(println (hello))
+(println (str
+            "the doc:\n" (hello)))
