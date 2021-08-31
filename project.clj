2350ead518cf64577868c6ec11d6f3853dc065af
tankorsmash <tankorsmash@gmail.com>
2021-08-31 11:38:08 -0400

    add html module

--- project.clj
+++ project.clj
@@ -2,9 +2,12 @@
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :profiles
     {:dev
-      {:dependencies [[org.clojure/clojurescript "1.10.773"]
+      {:resource-paths ["target"]
+       :clean-targets ^{:protect false} ["target"]
+       :dependencies [[org.clojure/clojurescript "1.10.773"]
                       [com.bhauman/figwheel-main "0.2.14"]
                       [org.slf4j/slf4j-simple "1.7.5"]
+                      [hiccup "1.0.5"]
                       ;; optional but recommended
                       [com.bhauman/rebel-readline-cljs "0.1.4"]]}}
   :aliases {"fig" ["trampoline" "run" "-m" "figwheel.main"]})
