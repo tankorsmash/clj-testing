86847651784117c8f5e1dd015d8680a0434a7c2b
tankorsmash <tankorsmash@gmail.com>
2021-09-01 17:15:49 -0400

    move non-dev deps into global dependency

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
 (defproject clj-testing "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.slf4j/slf4j-simple "1.7.5"]
+                 [cljs-http "0.1.46"]
+                 [hiccups "0.3.0"]]
   :profiles
     {:dev
       {:resource-paths ["target"]
        :clean-targets ^{:protect false} ["target"]
        :dependencies [[org.clojure/clojurescript "1.10.773"]
                       [com.bhauman/figwheel-main "0.2.14"]
-                      [org.slf4j/slf4j-simple "1.7.5"]
                       [cider/piggieback "0.4.2"]
                       [org.clojure/test.check "0.10.0"]
-                      [cljs-http "0.1.46"]
-                      [hiccups "0.3.0"]
                       ;; optional but recommended
                       [com.bhauman/rebel-readline-cljs "0.1.4"]]}}
   :aliases {"fig" ["trampoline" "run" "-m" "figwheel.main"]
