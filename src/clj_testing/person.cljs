877b9620d5f44ab94e06a9a9e1f16a514580d730
tankorsmash <tankorsmash@gmail.com>
2021-09-02 15:32:13 -0400

    extra to-valid-person to allow for :post validity checks

--- src/clj_testing/person.cljs
+++ src/clj_testing/person.cljs
@@ -37,39 +37,39 @@
 
 
 (declare with-valid-person)
+(declare to-valid-person)
 
 
-(defn is-map-true [person the_fn]
+(defn is-map-true [person]
   (if (s/valid? :person/isValidUnq person)
-      (do (println "is isValidUnq") (the_fn (unq-to-person person)))
+      (do (println "is isValidUnq") (unq-to-person person))
       (do (println "is NOT isValidUnq")
           (str "Not a valid person because its a map but not a person map: " (pprint-person person))
-          {:person/age 0 :person/name ""})))
-(defn is-map-false [person the_fn]
+          {:person/age 0 :person/name "Mr. Valid B. Empty"})))
+(defn is-map-false [person]
   (if (object? person)
-      (do (println "is object") (with-valid-person (js->clj person) the_fn))
+      (do (println "is object") (to-valid-person (js->clj person)))
       (do (println "is NOT object") (str (str "Not a valid person, unknown type" (type person)) person))))
 
-(defn is-isvalid-false [person the_fn]
+(defn is-isvalid-false [person]
   (if (instance? Person person)
-      (do (println "is Person") (the_fn (rec-to-person person)))
+      (do (println "is Person") (rec-to-person person))
       (do (println "is NOT Person") (if (map? person)
-                                        (do (println "so its a map???") ( is-map-true person the_fn))
-                                        (is-map-false person the_fn)))))
+                                        (do (println "so its a map???") ( is-map-true person))
+                                        (is-map-false person)))))
 
 (defn to-valid-person [person]
-   {:post [(clojure.test/is (s/valid? :person/isValid %))]}) ;;NOTE this doesnt work because this function also returns the (the_fn person)
+   {:post [(clojure.test/is (s/valid? :person/isValid %))]} ;;NOTE this doesnt work because this function also returns the (the_fn person)
+   (if (s/valid? :person/isValid person)
+       (do (println "is isValid") person)
+       (do (println "is NOT isValid") (is-isvalid-false person))))
 
 (defn with-valid-person
+  ;; ^{:doc "calls `(the_fn person)` if its a valid person, Person, or JSON Person"}
   [person the_fn]
-  ;; (let [valid-person (to-valid-person person)]
-  ;;   (the_fn valid-person))
-  {:doc "calls `(the_fn person)` if its a valid person, Person, or JSON Person"}
-   ;; :post [(clojure.test/is (s/valid? :person/isValid %))]} ;;NOTE this doesnt work because this function also returns the (the_fn person)
   (println "with-valid-person with :" person)
-  (if (s/valid? :person/isValid person)
-      (do (println "is isValid") (the_fn person))
-      (do (println "is NOT isValid") (is-isvalid-false person the_fn))))
+  (let [valid-person (to-valid-person person)]
+      (the_fn valid-person)))
 
 (defn tryget-person-name [person]
   {:doc "tries real hard to get a person's name for debugging purposes"}
