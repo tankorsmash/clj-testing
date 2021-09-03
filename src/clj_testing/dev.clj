0614c60e2bcbf2de788ae8b967eab2740c8adad0
tankorsmash <tankorsmash@gmail.com>
2021-09-03 13:19:21 -0400

    add clj-testing.dev file that you can include when at the repl to launch figwheel

---
+++ src/clj_testing/dev.clj
@@ -0,0 +1,15 @@
+(ns clj-testing.dev)
+
+;; IDK why this needs to be done at the toplevel and throws a syntax error if its in a function
+;; (defn setup-vim-cljs-repl []
+;; (do (require 'figwheel.main.api)
+;;     (figwheel.main.api/start {:mode :serve} "dev")
+;;     (figwheel.main.api/cljs-repl "dev"))
+
+;; it actually needs to be done in a macro so that vim's Piggieback can access it. IDK why either
+(defmacro set_up_vim_outer_repl []
+  `(do (require 'figwheel.main.api)
+      (figwheel.main.api/start {:mode :serve} "dev")
+      (figwheel.main.api/cljs-repl "dev")))
+
+(set_up_vim_outer_repl)
