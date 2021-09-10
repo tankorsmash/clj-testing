fd32c692d6d2164b1f23bcf9710384fa077756d7
tankorsmash <tankorsmash@gmail.com>
2021-09-10 17:36:12 -0400

    conver the parsed fields into a vector for printing
    PersistentArrayMap doesn't let you call first on it apparently. idk

--- scripts/bb_testing.clj
+++ scripts/bb_testing.clj
@@ -2,18 +2,25 @@
 
 (require '[clojure.java.shell :refer [sh]]
          '[clojure.string :as str]
-         '[cheshire.core :refer :all])
+         '[cheshire.core :refer [parse-string]])
 
 
 (defn node [& args]
   (apply sh "node" args))
 
 
+(defn parse-field [{:keys [attrName prettyName type] :as field}]
+  ;; (println attrName " - " prettyName " <> " type)
+  (println field))
+
 (defn handle-mapper-json [mapper-json]
   (let [parsed-mapper (parse-string mapper-json true)
-        filename (first (keys parsed-mapper))]
+        filename (first (keys parsed-mapper))
+        fields (filename parsed-mapper)]
     (println "The filename of the mapped file is:" (name filename))
-    (println "the body of the mapper is:\n" (filename parsed-mapper))))
+    ;; (println "the body of the mapper is:\n" (filename parsed-mapper))
+    ;; (parse-field (first (vec fields)))))
+    (map parse-field (vec fields))))
 
 (defn main []
   ;; (sh "node" "scripts/mapper_parsing.js" "weaponMapper.js")
