aa6fb9e9b2da68425453d1b5d9ae10cc3f485a16
tankorsmash <tankorsmash@gmail.com>
2021-09-12 22:29:58 -0400

    dump json via clojure.json

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
                  [cljsjs/react-dom "17.0.2-0"]
                  [reagent-catch "0.1.2"]
                  [clj-commons/secretary "1.2.4"]
-                 [org.clojure/core.match "0.3.0"]]
+                 [org.clojure/core.match "0.3.0"]
+                 [org.clojure/data.json "2.4.0"]]
 
   :compiler {:external-config {:devtools/config
                                {:print-config-overrides true
--- src/clj_testing/server.clj
+++ src/clj_testing/server.clj
@@ -1,7 +1,8 @@
 (ns clj-testing.server
   (:require
    [ring.util.response :refer [resource-response content-type not-found]]
-   [clojure.spec.alpha :as s]))
+   [clojure.spec.alpha :as s]
+   [clojure.data.json :as json]))
 
 ;; the routes that we want to be resolved to index.html
 (def route-set #{"/" "/contact" "/menu" "/about /testme" "/ajax"})
@@ -15,10 +16,18 @@
    :headers {"Content-Type" "text/html"}
    :body "This is a homemade custom path"})
 
+(defn handler-ajax [req]
+  {:status 200
+   :headers {"Content-Type" "application/json"}
+   ;; :body "{'success': true, 'message': 'THIS WAS A SUCCESS!'}"
+   :body (json/write-str {:success true :message "THIS WAS A SUCCESS FROM JSON!"})})
+
 
 (defn handler [req]
   (or
    (when (route-set (:uri req))
-     (home-page req))
+     (if (= (:uri req) "/ajax")
+       (handler-ajax req)
+       (home-page req)))
    (handler404 req)))
    ;; (home-page req)))
