04dac835173a81d422b2bd50a772ad2ef5c79e0c
tankorsmash <tankorsmash@gmail.com>
2021-09-15 21:12:44 -0400

    thought I had the defkeys thing sorted for passing a list of keys to s/def but no dice

--- src/spec_utils.clj
+++ src/spec_utils.clj
@@ -16,10 +16,38 @@
 (defmacro define-spec [spec-key validator2]
   `(eval `(s/def ~~spec-key ~~validator2)))
 
-
+(defmacro defspec [k spec-form]
+  `(s/def-impl ~k (quote ~spec-form) ~spec-form))
 (comment
+
+  (defmacro defkeys [req-keys]
+    `(s/keys :req ~@req-keys))
+
   (def my-key :frame-data.weapon/poop)
   (def validator string?)
+
+  (def new-key :test/pee)
+  (def form-validator (s/coll-of string?))
+  (defspec new-key form-validator)
+  (s/describe new-key)
+  (s/valid? new-key "ASDASD")
+  (s/valid? new-key 123)
+  (s/valid? new-key ["asd"])
+  (s/valid? new-key ["asd", 123])
+
+  (def required-keys [:frame-data.weapon/pretty_name])
+  ;; (s/def ::list-of-keys (s/keys :req-un [:frame-data.weapon/pretty_name]))
+  (s/def ::list-of-keys (s/keys :req-un required-keys))
+  (s/def ::dyn-list-of-keys (defkeys required-keys))
+  (s/describe ::dyn-list-of-keys)
+  (s/valid? ::list-of-keys {})
+  (s/valid? ::list-of-keys {:pretty_name "asd"})
+  (def map-key :test/map)
+  (defspec map-key ::list-of-keys)
+  (s/valid? map-key {})
+  (s/valid? map-key {:pretty_name "ASD"})
+  ;; (def map-validator (s/keys))
+
   (define-spec my-key validator)
   (s/def (eval `my-key) validator)
   (s/describe my-key)
