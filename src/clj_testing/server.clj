55053ee52d5af4647993b0f46ed23edc92ca9c06
tankorsmash <tankorsmash@gmail.com>
2021-09-16 18:06:38 -0400

    zprint formatted
    java -jar ./zprint-filter -w "../../clj_testing/src/clj_testing/server.clj"

--- src/clj_testing/server.clj
+++ src/clj_testing/server.clj
@@ -1,117 +1,117 @@
 (ns clj-testing.server
-  (:require
-   [spec-utils :refer [define-spec def-keys-req def-keys-req-un]]
-   [clojure.string :as string]
-   [clojure.pprint :refer [pprint]]
-   [ring.util.response :refer [resource-response content-type not-found]]
-   [reitit.ring :as ring]
-   [reitit.spec :as rs]
-   [reitit.dev.pretty :as pretty]
-   [clojure.spec.alpha :as s]
-   [clojure.data.json :as json]
-   [clj-http.client :as client]
-   [clojure.java.shell :refer [sh]]
-   [clojure.core.match :refer [match]]))
+  (:require [spec-utils :refer [define-spec def-keys-req def-keys-req-un]]
+            [clojure.string :as string]
+            [clojure.pprint :refer [pprint]]
+            [ring.util.response :refer
+             [resource-response content-type not-found]]
+            [reitit.ring :as ring]
+            [reitit.spec :as rs]
+            [reitit.dev.pretty :as pretty]
+            [clojure.spec.alpha :as s]
+            [clojure.data.json :as json]
+            [clj-http.client :as client]
+            [clojure.java.shell :refer [sh]]
+            [clojure.core.match :refer [match]]))
 
 ;; the routes that we want to be resolved to index.html
 (def route-set #{"/" "/contact" "/menu" "/about /testme" "/ajax"})
 
-(defn home-page [req]
+(defn home-page
+  [req]
   (some-> (resource-response "index.html" {:root "public"})
           (content-type "text/html; charset=utf-8")))
 
 (defn handler404
-  ([] {:status 404
-       :headers {"Content-Type" "text/html"}
-       :body "This is a homemade custom 404 path without args"})
-  ([req] {:status 404
-          :headers {"Content-Type" "text/html"}
-          :body "This is a arg-driven custom 404 path"})
-  ([req message] {:status 404
-                  :headers {"Content-Type" "text/html"}
-                  :body message}))
+  ([]
+   {:status 404,
+    :headers {"Content-Type" "text/html"},
+    :body "This is a homemade custom 404 path without args"})
+  ([req]
+   {:status 404,
+    :headers {"Content-Type" "text/html"},
+    :body "This is a arg-driven custom 404 path"})
+  ([req message]
+   {:status 404, :headers {"Content-Type" "text/html"}, :body message}))
 
 (defonce frame-types-to-filename
-  {:weapon "all_weapon_frames.json"
-   :armor "all_armor_frames.json"
-   :zone "all_zone_frames.json"
-   :weapon_category "all_weapon_category_frames.json"
-   :attribute "all_attribute_frames.json"
-   :battle_text_struct "all_battle_text_struct_frames.json"})
+         {:weapon "all_weapon_frames.json",
+          :armor "all_armor_frames.json",
+          :zone "all_zone_frames.json",
+          :weapon_category "all_weapon_category_frames.json",
+          :attribute "all_attribute_frames.json",
+          :battle_text_struct "all_battle_text_struct_frames.json"})
 
 (def root-static-asset-dir
   "C:\\Users\\Josh\\Documents\\cocos_projects\\magnolia_cocos\\Resources\\static_asset_dir")
 
-(defn handler-redirect [req to-uri]
-  {:status 302
-   :headers {"Location" to-uri}})
+(defn handler-redirect [req to-uri] {:status 302, :headers {"Location" to-uri}})
 
-(defn handler-ajax [req]
+(defn handler-ajax
+  [req]
   ;; (let [txt (json/read-str (:body (client/get "http://httpbin.org/get")))])
   (let [txt "ASDASD"]
-    {:status 200
-     :headers {"Content-Type" "application/json"}
-     :body (json/write-str {:success true :message txt})}))
+    {:status 200,
+     :headers {"Content-Type" "application/json"},
+     :body (json/write-str {:success true, :message txt})}))
 
-(defn debug-handler [req]
+(defn debug-handler
+  [req]
   (let [match (:reitit.core/match req)]
-    {:status 200
-     :headers {"Content-Type" "application/json"}
-     :body (json/write-str {:success true
-                            :message {:match-name (get-in match [:data :name])
+    {:status 200,
+     :headers {"Content-Type" "application/json"},
+     :body (json/write-str {:success true,
+                            :message {:match-name (get-in match [:data :name]),
                                       :match-path (get-in match [:path])}})}))
 
-(defn default-handler [] ;;reitit doesnt give you a request for these for some reason i cant possibly understand
-  {:status 200
-   :headers {"Content-Type" "application/json"}
-   :body (json/write-str {:success true
-                          :message "WTFFF"})})
+(defn default-handler
+  [] ;;reitit doesnt give you a request for these for some reason i cant
+     ;;possibly understand
+  {:status 200,
+   :headers {"Content-Type" "application/json"},
+   :body (json/write-str {:success true, :message "WTFFF"})})
 
-(defn test-handler [req]
-  {:status 200
-   :headers {"Content-Type" "application/json"}
-   :body (json/write-str {:success true
-                          :message "TEST WTFFF"})})
+(defn test-handler
+  [req]
+  {:status 200,
+   :headers {"Content-Type" "application/json"},
+   :body (json/write-str {:success true, :message "TEST WTFFF"})})
 
 (defn valid-json-response
   "Returns a valid JSON response.
   Expects a message string, and data of any type"
   ([data]
-   {:status 200
-    :headers {"Content-Type" "application/json"}
-    :body (json/write-str {:success true
-                           :message "Request successful"
-                           :data data})})
+   {:status 200,
+    :headers {"Content-Type" "application/json"},
+    :body (json/write-str
+            {:success true, :message "Request successful", :data data})})
   ([message data]
-   {:status 200
-    :headers {"Content-Type" "application/json"}
-    :body (json/write-str {:success true
-                           :message message
-                           :data data})}))
+   {:status 200,
+    :headers {"Content-Type" "application/json"},
+    :body (json/write-str {:success true, :message message, :data data})}))
 
-(defn get-by-frame-type [req]
+(defn get-by-frame-type
+  [req]
   (let [match (:reitit.core/match req)
         frame-type (get-in match [:path-params :frame-type])]
     (if-not (contains? frame-types-to-filename (keyword frame-type))
       (handler404 req (str "Unknown frame-type: " frame-type))
-      (do
-        (let [str-frame-data (slurp
-                              (str root-static-asset-dir
-                                   "\\"
-                                   ((keyword frame-type) frame-types-to-filename)))
-              frame-data (json/read-str str-frame-data)]
-          (valid-json-response  frame-data))))))
+      (do (let [str-frame-data (slurp (str root-static-asset-dir
+                                           "\\"
+                                           ((keyword frame-type
+                                             frame-types-to-filename))))
+                frame-data (json/read-str str-frame-data)]
+            (valid-json-response frame-data))))))
 
-(defn frame-ids-match? [frame other-frame]
-  (= (:frame_id frame)
-     (:frame_id other-frame)))
+(defn frame-ids-match?
+  [frame other-frame]
+  (= (:frame_id frame) (:frame_id other-frame)))
 
-(defn matching-frame [all-frames target-frame]
-  (filter #(= (:frame_id %)
-              (:frame_id target-frame))
-          all-frames))
+(defn matching-frame
+  [all-frames target-frame]
+  (filter #(= (:frame_id %) (:frame_id target-frame)) all-frames))
 
-(defn update-existing-frames [all-frames new-frame]
+(defn update-existing-frames
+  [all-frames new-frame]
   "takes all-frames and a single new-frame, then either appends it
   or updates the existing frames"
   (let [matching-frames (matching-frame all-frames new-frame)]
@@ -121,33 +121,27 @@
              (if (frame-ids-match? existing-frame new-frame)
                (merge existing-frame new-frame)
                existing-frame))
-           all-frames)))) ;;update the matching ones
+        all-frames))));;update the matching ones
 
-(defn add-missing-slash [uri]
+(defn add-missing-slash
+  [uri]
   (let [endswith-slash? (string/ends-with? uri "/")]
-    (if (not endswith-slash?)
-      (str uri "/")
-      uri)))
+    (if (not endswith-slash?) (str uri "/") uri)))
 
 (def handler
   (ring/ring-handler
-   (ring/router
-    ["/" ::home
-     ["api/"
-      ["frames/"
-       ["" {:name ::frames-home :get test-handler}]
-       [":frame-type/" {:name ::frames-frame-type :get get-by-frame-type}]]]])
+    (ring/router ["/" ::home
+                  ["api/"
+                   ["frames/" ["" {:name ::frames-home, :get test-handler}]
+                    [":frame-type/"
+                     {:name ::frames-frame-type, :get get-by-frame-type}]]]])
+    (ring/routes (ring/redirect-trailing-slash-handler)
+                 (ring/create-default-handler {:not-found handler404}))))
 
-   (ring/routes
-    (ring/redirect-trailing-slash-handler)
-    (ring/create-default-handler
-     {:not-found handler404}))))
+(defn node [& args] (apply sh "node" args))
 
-(defn node [& args]
-  (apply sh "node" args))
-
-(defn parse-field [spec-ns
-                   {:keys [attrName prettyName type] :as field}]
+(defn parse-field
+  [spec-ns {:keys [attrName prettyName type], :as field}]
   (let [vvvalidator (match [type]
                       ["string"] #'string?
                       ["string[]"] '(s/coll-of string?)
@@ -160,7 +154,8 @@
       (prn "The new spec: " new-def)
       new-def)))
 
-(defn handle-mapper-json [mapper-json namespace_]
+(defn handle-mapper-json
+  [mapper-json namespace_]
   (let [parsed-mapper (json/read-str mapper-json :key-fn keyword)
         filename (first (keys parsed-mapper))
         fields (filename parsed-mapper)]
@@ -169,15 +164,14 @@
       (pprint fields)
       (println "Registered count specs:" (count field-specs))
       (println "...and wrapped keys in:"
-               (define-spec
-                 (keyword namespace_ "frame")
-                 (def-keys-req field-specs)))
+               (define-spec (keyword namespace_ "frame")
+                            (def-keys-req field-specs)))
       (println "...and wrapped keys in un:"
-               (define-spec
-                 (keyword namespace_ "frame-un")
-                 (def-keys-req-un field-specs))))))
+               (define-spec (keyword namespace_ "frame-un")
+                            (def-keys-req-un field-specs))))))
 
-(defn register-specs [filename namespace_]
+(defn register-specs
+  [filename namespace_]
   (let [result (node "scripts/mapper_parsing.js" filename)]
     (if (zero? (:exit result))
       (handle-mapper-json (:out result) namespace_)
@@ -188,53 +182,135 @@
     (if (zero? (:exit result))
       (handle-mapper-json (:out result))
       (println "\nERROR!!!\n\n" result)))
-
-  (do
-    (register-specs "weaponMapper.js" "frame-data.weapon")
-    (register-specs "armorMapper.js" "frame-data.armor")
-    (register-specs "zoneMapper.js" "frame-data.zone")
-    (register-specs "weaponCategoryMapper.js" "frame-data.weapon-category")
-    (register-specs "attributeMapper.js" "frame-data.attribute")
-    (register-specs "battleTextStructMapper.js" "frame-data.battle-text-struct"))
-
+  (do (register-specs "weaponMapper.js" "frame-data.weapon")
+      (register-specs "armorMapper.js" "frame-data.armor")
+      (register-specs "zoneMapper.js" "frame-data.zone")
+      (register-specs "weaponCategoryMapper.js" "frame-data.weapon-category")
+      (register-specs "attributeMapper.js" "frame-data.attribute")
+      (register-specs "battleTextStructMapper.js"
+                      "frame-data.battle-text-struct"))
   (s/describe :frame-data.zone/location_data_names_in_the_zone)
   (s/describe :frame-data.zone/frame)
   (s/describe :frame-data.armor/frame)
-
   (client/head "http://httpbin.org/get")
-
   (map #(ns-unmap *ns* %) (keys (ns-interns *ns*))) ;;clean namespace entirely
-
   (def all-weapon-frames
-    [{:bonus_attack 1, :description "", :frame_id 1, :pretty_name "TEST SPEARss test TEST!", :rarity_type 0, :damage_type 2, :bonus_encumbrance 4, :frame_image_path "combined_spear.png", :affects_morale false, :carry_weight 4, :bonus_power 0, :battle_row_type 1} {:bonus_attack 1, :description "", :frame_id 2, :pretty_name "TEST WEAPON whose category is without specified attributes", :rarity_type 0, :damage_type 2, :bonus_encumbrance 4, :frame_image_path "combined_spear.png", :affects_morale false, :carry_weight 4, :bonus_power 0, :battle_row_type 1} {:bonus_attack 1, :description "", :frame_id 1000, :pretty_name "Spear", :rarity_type 0, :damage_type 2, :bonus_encumbrance 4, :frame_image_path "combined_spear.png", :affects_morale false, :carry_weight 4, :bonus_power 0, :battle_row_type 1} {:bonus_attack 1, :description "", :frame_id 1001, :pretty_name "Shortbow", :rarity_type 0, :damage_type 1, :bonus_encumbrance 4, :frame_image_path "combined_shortbow.png", :affects_morale false, :carry_weight 2, :bonus_power 0, :battle_row_type 1} {:bonus_attack 0, :description "", :frame_id 1002, :pretty_name "Claw", :rarity_type 0, :damage_type 3, :bonus_encumbrance 5, :frame_image_path "combined_claw.png", :affects_morale false, :carry_weight 0, :bonus_power 1, :battle_row_type 0} {:bonus_attack 1, :description "", :frame_id 1003, :pretty_name "Flail", :rarity_type 0, :damage_type 3, :bonus_encumbrance 8, :frame_image_path "combined_flail.png", :affects_morale false, :carry_weight 10, :bonus_power 2, :battle_row_type 0} {:bonus_attack -1, :description "", :frame_id 1004, :pretty_name "Two-Handed Club", :rarity_type 0, :damage_type 2, :bonus_encumbrance 5, :frame_image_path "combined_oaken_club.png", :affects_morale false, :carry_weight 4, :bonus_power 2, :battle_row_type 0} {:bonus_attack 1, :description "", :frame_id 1005, :pretty_name "Arming Sword (Ulfburt)", :rarity_type 1, :damage_type 3, :bonus_encumbrance 2, :frame_image_path "combined_blunt_cutlass.png", :affects_morale false, :carry_weight 3, :bonus_power 0, :battle_row_type 0}])
-
+    [{:bonus_attack 1,
+      :description "",
+      :frame_id 1,
+      :pretty_name "TEST SPEARss test TEST!",
+      :rarity_type 0,
+      :damage_type 2,
+      :bonus_encumbrance 4,
+      :frame_image_path "combined_spear.png",
+      :affects_morale false,
+      :carry_weight 4,
+      :bonus_power 0,
+      :battle_row_type 1}
+     {:bonus_attack 1,
+      :description "",
+      :frame_id 2,
+      :pretty_name "TEST WEAPON whose category is without specified attributes",
+      :rarity_type 0,
+      :damage_type 2,
+      :bonus_encumbrance 4,
+      :frame_image_path "combined_spear.png",
+      :affects_morale false,
+      :carry_weight 4,
+      :bonus_power 0,
+      :battle_row_type 1}
+     {:bonus_attack 1,
+      :description "",
+      :frame_id 1000,
+      :pretty_name "Spear",
+      :rarity_type 0,
+      :damage_type 2,
+      :bonus_encumbrance 4,
+      :frame_image_path "combined_spear.png",
+      :affects_morale false,
+      :carry_weight 4,
+      :bonus_power 0,
+      :battle_row_type 1}
+     {:bonus_attack 1,
+      :description "",
+      :frame_id 1001,
+      :pretty_name "Shortbow",
+      :rarity_type 0,
+      :damage_type 1,
+      :bonus_encumbrance 4,
+      :frame_image_path "combined_shortbow.png",
+      :affects_morale false,
+      :carry_weight 2,
+      :bonus_power 0,
+      :battle_row_type 1}
+     {:bonus_attack 0,
+      :description "",
+      :frame_id 1002,
+      :pretty_name "Claw",
+      :rarity_type 0,
+      :damage_type 3,
+      :bonus_encumbrance 5,
+      :frame_image_path "combined_claw.png",
+      :affects_morale false,
+      :carry_weight 0,
+      :bonus_power 1,
+      :battle_row_type 0}
+     {:bonus_attack 1,
+      :description "",
+      :frame_id 1003,
+      :pretty_name "Flail",
+      :rarity_type 0,
+      :damage_type 3,
+      :bonus_encumbrance 8,
+      :frame_image_path "combined_flail.png",
+      :affects_morale false,
+      :carry_weight 10,
+      :bonus_power 2,
+      :battle_row_type 0}
+     {:bonus_attack -1,
+      :description "",
+      :frame_id 1004,
+      :pretty_name "Two-Handed Club",
+      :rarity_type 0,
+      :damage_type 2,
+      :bonus_encumbrance 5,
+      :frame_image_path "combined_oaken_club.png",
+      :affects_morale false,
+      :carry_weight 4,
+      :bonus_power 2,
+      :battle_row_type 0}
+     {:bonus_attack 1,
+      :description "",
+      :frame_id 1005,
+      :pretty_name "Arming Sword (Ulfburt)",
+      :rarity_type 1,
+      :damage_type 3,
+      :bonus_encumbrance 2,
+      :frame_image_path "combined_blunt_cutlass.png",
+      :affects_morale false,
+      :carry_weight 3,
+      :bonus_power 0,
+      :battle_row_type 0}])
   (def single-zone-frame
-    {:name "The Greater Capital Area"
-     :data_name "the_greater_capital_area"
-     :required_zone_data_name_to_unlock ""
-     :location_data_names_in_the_zone ["the_forest" "the_mountains" "the_plains"]})
-
+    {:name "The Greater Capital Area",
+     :data_name "the_greater_capital_area",
+     :required_zone_data_name_to_unlock "",
+     :location_data_names_in_the_zone ["the_forest" "the_mountains"
+                                       "the_plains"]})
   (def invalid-raw-single-zone-frame-json
     "{ \"required_zone_data_name_to_unlock\": \"\", \"location_data_names_in_the_zone\": [ \"the_forest\", \"the_mountains\", \"the_plains\" ] }")
   (def invalid-single-zone-frame
     (json/read-str invalid-raw-single-zone-frame-json :key-fn keyword))
-
   (s/describe :frame-data.zone/frame-un)
   (s/valid? :frame-data.zone/frame-un single-zone-frame)
   (s/valid? :frame-data.zone/frame-un invalid-single-zone-frame)
   (s/explain-data :frame-data.zone/frame-un invalid-single-zone-frame)
   (s/valid? :frame-data.zone/frame-un {})
   (s/valid? :frame-data.zone/frame-un 0)
-
-  (def new-weapon-frame-to-add
-    {:frame_id 1
-     :pretty_name "newly added frame"})
-
+  (def new-weapon-frame-to-add {:frame_id 1, :pretty_name "newly added frame"})
   (first (update-existing-frames all-weapon-frames new-weapon-frame-to-add))
-
   (s/describe my-key)
   (s/describe :frame-data.weapon/affects_morale)
   (s/valid? :frame-data.weapon/frame_id 123)
   (s/valid? :frame-data.weapon/frame_id "ASD")
-
   (s/def :frame-data.weapon/pretty_name123 string?))
