2769d599a0ef4a2775cb9871193c328075c8915c
tankorsmash <tankorsmash@gmail.com>
2021-09-18 20:19:27 -0400

    add volume setting script i can hook into with my server

---
+++ scripts/volume_setter.py
@@ -0,0 +1,28 @@
+import argh
+
+from pycaw.pycaw import AudioUtilities
+
+
+def set_volume_to_pct(volume, vol_pct):
+    if vol_pct > 1.0:
+        print("Volume can't be higher than 1.0")
+        return
+
+    volume.SetMasterVolume(vol_pct, None)
+
+def filter_sessions_by_procname(procname):
+    sessions = AudioUtilities.GetAllSessions()
+    matches = list(filter(lambda s: s.Process and s.Process.name() == procname,
+                          sessions))
+    print(f"num matches {len(matches)} out of {len(sessions)} for '{procname}'")
+
+    return matches
+
+def set_volume_for_procs(procname="Discord.exe", vol_pct=1.0):
+    for session in filter_sessions_by_procname("Discord.exe"):
+        volume = session.SimpleAudioVolume
+        set_volume_to_pct(volume, vol_pct)
+
+
+if __name__ == "__main__":
+    argh.dispatch_command(set_volume_for_procs)
